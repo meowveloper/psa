1803075bb51710698a6493f8c57cbf6e8fbb5196
meowveloper <meowveloper.dev@gmail.com>
2025-11-13 20:11:07 +0700

    parsing

--- src/constants.zig
+++ src/constants.zig
@@ -6,3 +6,9 @@ pub const usage_string =
     \\    Example: psa -m=dict -h=5f4dcc3b5aa765d61d8327deb882cf99 -w=rockyou.txt
     \\
 ;
+
+
+pub const dict_mode_string = "dict";
+pub const brute_mode_string = "brute";
+pub const audit_mode_string = "audit";
+
--- src/main.zig
+++ src/main.zig
@@ -10,31 +10,44 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-
-    if(args.len < 2) {
+    if (args.len < 2) {
         std.debug.print("{s}\n", .{constants.usage_string});
         return;
     }
 
     var mode: ?[]const u8 = null;
     var hash: ?[]const u8 = null;
+    var wordlist: ?[]const u8 = null;
 
-    for(args[1..]) |arg| {
-        if(std.mem.startsWith(u8, arg, "-m=")) {
+    for (args[1..]) |arg| {
+        if (std.mem.startsWith(u8, arg, "-m=")) {
             mode = arg[3..];
         }
-        if(std.mem.startsWith(u8, arg, "-h=")) {
+        if (std.mem.startsWith(u8, arg, "-h=")) {
             hash = arg[3..];
+        }
+        if (std.mem.startsWith(u8, arg, "-w=")) {
+            wordlist = arg[3..];
         }
     }
 
-    if(mode) |m| {
-        std.debug.print("    mode: {s}\n", .{m});
-    } else {
-        std.debug.print("    ERROR!!!: mode required.", .{});
+    if (mode == null) {
+        std.debug.print("    Error: -m required.\n", .{});
         return;
     }
-    if(hash) |h| std.debug.print("    hash: {s}\n", .{h});
+
+    const valid_mode = std.mem.eql(u8, mode.?, constants.dict_mode_string) or
+        std.mem.eql(u8, mode.?, constants.brute_mode_string) or
+        std.mem.eql(u8, mode.?, constants.audit_mode_string);
+
+    if (!valid_mode) {
+        std.debug.print("    Error: invalid mode {s}. Must be {s}, {s}, or {s}.\n", .{ mode.?, constants.dict_mode_string, constants.brute_mode_string, constants.audit_mode_string });
+        return;
+    }
+
+    std.debug.print("    mode: {s}\n", .{mode.?});
+    if (hash) |h| std.debug.print("    hash: {s}\n", .{h});
+    if (wordlist) |w| std.debug.print("    wordlist: {s}\n", .{w});
 }
 
 test "basic arg alloc" {
@@ -50,25 +63,23 @@ test "basic arg alloc" {
 }
 
 test "arg parsing" {
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
     const allocator = gpa.allocator();
 
-
-    const test_args = [_][]const u8{"psa", "-m=dict"};
+    const test_args = [_][]const u8{ "psa", "-m=dict" };
     const args = try allocator.dupe([]const u8, &test_args);
     defer allocator.free(args);
 
     var mode: ?[]const u8 = null;
     var hash: ?[]const u8 = null;
 
-    for(args[1..]) |arg| {
-        if(std.mem.startsWith(u8, arg, "-m=")) {
+    for (args[1..]) |arg| {
+        if (std.mem.startsWith(u8, arg, "-m=")) {
             mode = arg[3..];
         }
-        if(std.mem.startsWith(u8, arg, "-h=")) {
+        if (std.mem.startsWith(u8, arg, "-h=")) {
             hash = arg[3..];
         }
     }
