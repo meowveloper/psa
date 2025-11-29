045bcec744ac9965e6c14ebfe44d85763d7237a3
meowveloper <meowveloper.dev@gmail.com>
2025-11-29 19:34:59 +0700

    something

--- src/root.zig
+++ src/root.zig
@@ -21,3 +21,41 @@ pub fn add(a: i32, b: i32) i32 {
 test "basic add functionality" {
     try std.testing.expect(add(3, 7) == 10);
 }
+
+test "basic arg alloc" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    try std.testing.expect(args.len > 0);
+}
+
+test "arg parsing" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const test_args = [_][]const u8{ "psa", "-m=dict" };
+    const args = try allocator.dupe([]const u8, &test_args);
+    defer allocator.free(args);
+
+    var mode: ?[]const u8 = null;
+    var hash: ?[]const u8 = null;
+
+    for (args[1..]) |arg| {
+        if (std.mem.startsWith(u8, arg, "-m=")) {
+            mode = arg[3..];
+        }
+        if (std.mem.startsWith(u8, arg, "-h=")) {
+            hash = arg[3..];
+        }
+    }
+
+    try std.testing.expect(mode != null);
+    try std.testing.expect(std.mem.eql(u8, mode.?, "dict"));
+}
