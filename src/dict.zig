937db573174fac225b938421db5f17236f2000b1
meowveloper <meowveloper.dev@gmail.com>
2025-12-21 22:31:33 +0700

    md5 equal?

--- src/dict.zig
+++ src/dict.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn dictionary_attack(allocator: std.mem.Allocator, hash: ?[]const u8, wordlist: ?[]const u8) !void {
-    _ = allocator;
     std.debug.print("performing dictionary attack!!\n", .{});
     std.debug.print("hash: {s}, wordlist: {s}\n", .{ hash.?, wordlist.? });
 
@@ -16,5 +15,17 @@ pub fn dictionary_attack(allocator: std.mem.Allocator, hash: ?[]const u8, wordli
     while (try reader.interface.takeDelimiter('\n')) |line| {
         line_no += 1;
         std.debug.print("{d}--{s}\n", .{line_no, line});
+        const is_equal = try is_md5_hash_equal(allocator, line, hash.?);
+        std.debug.print("{}\n", .{is_equal});
     }
+}
+
+pub fn is_md5_hash_equal(allocaltor: std.mem.Allocator, str: []const u8, hash_str: []const u8) !bool {
+    var hasher = std.crypto.hash.Md5.init(.{});
+    hasher.update(str);
+    var output: [16]u8 = undefined;
+    hasher.final(&output);
+    const string = try std.fmt.allocPrint(allocaltor, "{x}", .{output});
+    defer allocaltor.free(string);
+    return std.mem.eql(u8, string, hash_str);
 }
